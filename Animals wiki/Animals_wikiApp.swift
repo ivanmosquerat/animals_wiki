2102501d750bc162635eeab3db1b37a2845119ce
Ivan Mosquera <ivanmosquerat13@gmail.com>
2021-02-09 17:43:26 +1100

    Initial Commit

---
+++ Animals wiki/Animals_wikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animals_wikiApp.swift
+//  Animals wiki
+//
+//  Created by Ivan Mosquera on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Animals_wikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
