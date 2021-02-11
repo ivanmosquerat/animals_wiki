fa185c451a7c30736f04af3f10fa1b3de1419f02
Ivan Mosquera <ivanmosquerat13@gmail.com>
2021-02-11 18:44:26 +1100

    List of animals on content view created. Animal model added.

--- Animals wiki/Extension/CodableBundleExtension.swift
+++ Animals wiki/Extension/CodableBundleExtension.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Bundle{
     
-    func decode(_ file: String) -> [CoverImage]{
+    func decode<T: Codable>(_ file: String) -> T {
         
         // Locate json file.
         guard let url = self.url(forResource: file, withExtension: nil) else {
@@ -22,7 +22,7 @@ extension Bundle{
         // Create decoder
         let decoder = JSONDecoder()
         // Create a property for decoded data.
-        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+        guard let loaded = try? decoder.decode(T.self, from: data) else {
             fatalError("Failed to decode data from bundle.")
         }
         
